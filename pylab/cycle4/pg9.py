79849f1d905ea73bf42e4e49ff98b43f598dca83
ShinePaul1202 <24mp2316@rit.ac.in>
2024-11-25 15:46:27 +0530

    Update pg9.py

--- pylab/cycle4/pg9.py
+++ pylab/cycle4/pg9.py
@@ -1,7 +1,16 @@
 def add_integers(*args):
+    """
+    Returns the sum of all integer arguments.
+
+    Parameters:
+    *args: Integers to be added.
+
+    Returns:
+    int: The sum of the integers.
+    """
     return sum(args)
 
 print(add_integers(1, 2, 3))
 print(add_integers(10, -5, 5))
-print(add_integers(100, 200))
+print(add_integers(100, 200)) 
 print(add_integers())
