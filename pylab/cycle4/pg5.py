c3d6ff4fd4dc0ba396899a462d7bfae03950a9d2
ShinePaul1202 <24mp2316@rit.ac.in>
2024-11-25 13:33:57 +0530

    Update pg5.py

--- pylab/cycle4/pg5.py
+++ pylab/cycle4/pg5.py
@@ -1,5 +1,4 @@
-num_terms = int(input("Enter the number of terms for powers of 2: "))
+lst=[int(num) for num in input("enter list elements(space seperated):").split()]
+powers_of_2 = list(map(lambda x: 2 ** x, lst))
 
-powers_of_2 = list(map(lambda x: 2 ** x, range(num_terms)))
-
-print(f"Powers of 2 up to {num_terms} terms: {powers_of_2}")
+print(f"\nPowers of 2: {powers_of_2}")
