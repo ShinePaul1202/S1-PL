f4f7da501b39d65e96fff86ca4f3a8a58b474982
starzz <24mp2316@rit.ac.in>
2024-12-06 05:22:04 +0530

    Update pg8.py

--- pylab/cycle4/pg8.py
+++ pylab/cycle4/pg8.py
@@ -1,23 +1,9 @@
-def factorial(num):
-    if num == 0 or num == 1:
-        return 1
-    else:
-        result = 1
-        for i in range(2, num + 1):
-            result *= i
-        return result
+def compare(S1, S2, n):
+    return S1[:n] == S2[:n]
 
-def permutations(n, r):
-    return factorial(n) // factorial(n - r)
+S1 = input("Enter the first string: ")
+S2 = input("Enter the second string: ")
+n = int(input("Enter the number of characters to compare: "))
 
-def combinations(n, r):
-    return factorial(n) // (factorial(r) * factorial(n - r))
-
-n = int(input("Enter the value of n (total objects): "))
-r = int(input("Enter the value of r (objects taken at a time): "))
-
-if r > n:
-    print("Invalid input: r should be less than or equal to n.")
-else:
-    print(f"The number of permutations (P({n}, {r})) is: {permutations(n, r)}")
-    print(f"The number of combinations (C({n}, {r})) is: {combinations(n, r)}")
+result = compare(S1, S2, n)
+print("Result:", result)
