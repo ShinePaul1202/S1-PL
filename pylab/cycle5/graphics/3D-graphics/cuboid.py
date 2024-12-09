0b46b1e32db0c9d1a53e1d7134c5156a4b7c9829
starzz <24mp2316@rit.ac.in>
2024-12-09 07:23:07 +0530

    Delete pylab/cycle5/graphics directory

--- pylab/cycle5/graphics/3D-graphics/cuboid.py
+++
@@ -1,7 +0,0 @@
-def area(length, width, height):
-    """Calculate the surface area of a cuboid."""
-    return 2 * (length * width + width * height + height * length)
-
-def volume(length, width, height):
-    """Calculate the volume of a cuboid."""
-    return length * width * height
